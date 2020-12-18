12fa275121ce835b81851c994b94756fe820033d
Buralis <72046129+Buralis@users.noreply.github.com>
2020-12-18 13:11:27 +0100

    Partie 2 Terminée

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Ken Kaneki'},
+    { id: 12, name: 'Levi Ackerman'},
+    { id: 13, name: 'Sasuke Uchiha'},
+    { id: 14, name: 'Inuyasha'},
+    { id: 15, name: 'Monkey D. Luffy'},
+    { id: 16, name: 'Jotaro Kujo'},
+    { id: 17, name: 'Light Yagami'},
+    { id: 18, name: 'Shôtarô Kaneda'},
+    { id: 19, name: 'Yami Sukehiro'},
+];
